a449ff39488acdaa083deefd6aef82b048f2409d
Chris <chris@verida.io>
2021-03-17 11:38:43 +1030

    Implement missing NEAR methods

--- src/chains/near.ts
+++ src/chains/near.ts
@@ -1,17 +1,38 @@
 const nearAPI = require('near-api-js');
+const nearSeedPhrase = require('near-seed-phrase');
 const _ = require('lodash');
 const bs58 = require('bs58');
 const nacl = require('tweetnacl');
 const crypto = require('crypto');
 
+function implicitAccountId(publicKey: string) {
+    return bs58.decode(publicKey.replace('ed25519:', '')).toString('hex');
+}
+
 export default class utils {
 
     static createWallet(): object {
-        throw new Error('Not implemented');
+        const { seedPhrase, secretKey, publicKey } = nearSeedPhrase.generateSeedPhrase();
+        const accountId = implicitAccountId(publicKey);
+
+        return {
+            mnemonic: seedPhrase,
+            privateKey: secretKey,
+            publicKey: publicKey,
+            address: accountId
+        }
     }
 
-    static getWallet(words: string): object {
-        throw new Error('Not implemented');
+    static getWallet(seedPhrase: string): object {
+        const { secretKey, publicKey } = nearSeedPhrase.parseSeedPhrase(seedPhrase)
+        const accountId = implicitAccountId(publicKey)
+
+        return {
+            mnemonic: seedPhrase,
+            privateKey: secretKey,
+            publicKey: publicKey,
+            address: accountId
+        }
     }
 
     static createPrivateKey(): string {
@@ -43,11 +64,12 @@ export default class utils {
      * @param message Message to sign
      */
     static async signMessage(privateKey: string, message: string): Promise<string> {
-        throw new Error('Not implemented');
-    }
-
-    static hexToBuffer(hex: string): Buffer {
-        return Buffer.from(hex.substring(2,hex.length), 'hex')
+        const { KeyPair } = nearAPI;
+        const keyPair = KeyPair.fromString(privateKey);
+        const messageBuffer = Buffer.from(message);
+        const hash = crypto.createHash('sha256').update(messageBuffer).digest();
+        const signature = keyPair.sign(hash);
+        return '0x' + Buffer.from(signature.signature).toString('hex');
     }
 
     /**
@@ -57,11 +79,14 @@ export default class utils {
      * @param signature 
      */
     static recoverAddress(message: string, signature: string) {
-        throw new Error('Not implemented');
+        throw new Error('Not supported due to NEAR\'s multi-key per account architecture');
     }
 
     /**
-     * Verify a signature matches a given DID
+     * Verify a signature matches a given DID.
+     * 
+     * This checks the blockchain to ensure the signature matches a valid public key
+     * attached to the on chain DID.
      * 
      * @param message 
      * @param signature  (hex encoded)
@@ -69,6 +94,29 @@ export default class utils {
      */
     static async verifySignature(message: string, signature: string, did: string, config: any = {}): Promise<boolean> {
         const accountId = did.replace('did:near:', '')
+        const { Account } = nearAPI;
+        const near = await utils.getNear(config)
+        
+        const nearAccount = new Account(near.connection, accountId);
+        const messageBuffer = Buffer.from(message);
+        const sigBuffer = Buffer.from(signature.replace('0x',''), 'hex')
+
+        // Referenced from https://github.com/near/near-contract-helper/blob/cf9bab1f05d3e639bb01c104cb465b35c89992b8/app.js#L133
+        try {
+            const hash = crypto.createHash('sha256').update(messageBuffer).digest();
+            const accessKeys = await nearAccount.getAccessKeys()
+            return accessKeys.some((it: any) => {
+                const publicKey = it.public_key.replace('ed25519:', '');
+                console.log("checking", publicKey);
+                return nacl.sign.detached.verify(hash, sigBuffer, bs58.decode(publicKey));
+            });
+        } catch (e) {
+            console.error(e);
+            return false;
+        }
+    }
+
+    static async getNear(config: any): Promise<any> {
         config = _.merge({
             networkId: 'default',
             nodeUrl: 'https://rpc.testnet.near.org',
@@ -76,27 +124,13 @@ export default class utils {
             helperUrl: 'https://helper.testnet.near.org',
         }, config);
 
-        const { Near, Account } = nearAPI;
+        const { Near } = nearAPI;
         const { networkId, nodeUrl, walletUrl } = config;
         const near = new Near({
             networkId, nodeUrl, walletUrl, deps: { keyStore: new nearAPI.keyStores.InMemoryKeyStore() }
         });
-        const nearAccount = new Account(near.connection, accountId);
-        const messageBuffer = Buffer.from(message);
-        const sigBuffer = Buffer.from(signature.replace('0x',''), 'hex')
 
-        // pulled from https://github.com/near/near-contract-helper/blob/cf9bab1f05d3e639bb01c104cb465b35c89992b8/app.js#L133
-        try {
-            const hash = crypto.createHash('sha256').update(messageBuffer).digest();
-            const accessKeys = await nearAccount.getAccessKeys()
-            return accessKeys.some((it: any) => {
-                const publicKey = it.public_key.replace('ed25519:', '');
-                return nacl.sign.detached.verify(hash, sigBuffer, bs58.decode(publicKey));
-            });
-        } catch (e) {
-            console.error(e);
-            return false;
-        }
+        return near;
     }
 
 }
