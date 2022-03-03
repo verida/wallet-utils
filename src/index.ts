4f743375fc862010535a3de25273b1b6d1d07e2d
Saad Ibrahim <saad.lakhany@gmail.com>
2022-03-03 23:04:16 +0400

    fix utils export issue

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const utils = require("./utils").default;
-const MultiChainWallet  = require("./MultiChainWallets").default;
+const utils = require("./utils").utils;
+const MultiChainWallet = require("./MultiChainWallets").default;
 
-export default {utils, MultiChainWallet}
+export default { utils, MultiChainWallet };
